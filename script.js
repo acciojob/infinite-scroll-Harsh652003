26cdb64d382005c7b3ec01b582722fe41cd24fb7
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-10 01:28:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-//your code here!
+document.addEventListener('DOMContentLoaded', () => {
+  const list = document.getElementById('infi-list');
+  let loading = false; 
+	
+  const addListItems = () => {
+    loading = true; 
+   
+    setTimeout(() => {
+      for (let i = 0; i < 17; i++) {
+        const li = document.createElement('li');
+        li.textContent = `Item ${list.children.length + 1}`; 
+        list.appendChild(li);
+      }
 
+      loading = false; 
+	}, 1000);
+  };
+
+   const handleScroll = () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 20 && !loading) {
+      addListItems();
+    }
+  };
+
+  
+  addListItems();
+
+  window.addEventListener('scroll', handleScroll);
+});
